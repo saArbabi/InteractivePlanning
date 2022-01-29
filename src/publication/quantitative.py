b4c9cbaaf2be5c32994576218e3b3865f73bc94e
Salar Arbabi <sa00443@surrey.ac.uk>
2022-01-29 10:53:25 +0000

    Clean up

--- src/publication/quantitative.py
+++ src/publication/quantitative.py
@@ -41,7 +41,8 @@ val_run_name = ['all_density']
 
 model_val_run_map = {
     'cae_003': val_run_name, #
-    'cae_014': val_run_name, #
+    # 'cae_014': val_run_name, #
+    'cae_010': val_run_name, #
     }
 # %%
 true_collections = {}
@@ -63,37 +64,11 @@ pred_collections[model_run_name].shape
 model_run_names = list(true_collections.keys())
 model_run_names
 # %%
-"""Compare different variants of the same model
-"""
-model_names = ['epoch_20',  'epoch_30', 'epoch_50']
-model_name = 'cae_004'
-true_collections = {}
-pred_collections = {}
-
-for val_run_name in model_names:
-    exp_dir = './src/models/experiments/'+model_name+'/' + val_run_name
-
-    with open(exp_dir+'/true_collections.pickle', 'rb') as handle:
-        true_collections[val_run_name] = np.array(pickle.load(handle))
-
-    with open(exp_dir+'/pred_collections.pickle', 'rb') as handle:
-        pred_collections[val_run_name] = np.array(pickle.load(handle))
-
-true_collections[val_run_name].shape
-pred_collections[val_run_name].shape
-pred_trace =
-true_collection[scene_sample, 0, 19:, indx_acts[0][0]+2]
-pred_collection[0, 0, :, indx_acts[0][0]]
-true_collection[scene_sample, 0, 19:, indx_acts[0][1]+2]
-pred_collection[0, 0, :, indx_acts[0][1]]
-
-
-# %%
 """
 Visualisation of model predictions. Use this for debugging.
 """
-model_run_name = 'cae_014_all_density'
-model_run_name = 'cae_003_all_density'
+model_run_name = 'cae_010_all_density'
+# model_run_name = 'cae_003_all_density'
 true_collection = true_collections[model_run_name]
 pred_collection = pred_collections[model_run_name]
 
@@ -102,8 +77,8 @@ traces_n = 10
 time_steps = np.linspace(0, 3.9, 40)
 veh_names = ['veh_m', 'veh_y', 'veh_f', 'veh_fadj']
 
-scene_samples = range(3)
-# scene_samples = [100, 150, 200]
+# scene_samples = range(3)
+scene_samples = [100, 150, 200, 250]
 for scene_sample in scene_samples:
     fig, axs = plt.subplots(figsize=(10, 1))
     start_time = true_collection[scene_sample, 0, 0, 1]
@@ -125,15 +100,7 @@ for scene_sample in scene_samples:
                 axs[v, s].plot(time_steps[19:], pred_trace,  color='grey')
                 # axs[veh_axis, state_axis].scatter(time_steps[19:][::5], pred_trace[::5],  color='black')
 # plt.savefig("test.png", dpi=500)
-# %%
-long_speed_err_collections['cae_003'].shape
-errs = long_speed_err_collections['cae_004'][:, -1]
-max_err = errs.max()
-np.where(errs == max_err)
 
-pred_collection[scene_sample, :, :, 0]
-pred_collection[132, :, :, indx_acts[0][0]]
-pred_collection[132, :, :, indx_acts[0][0]]
 # %%
 def get_trace_err(pred_traces, true_trace):
     """
@@ -142,7 +109,7 @@ def get_trace_err(pred_traces, true_trace):
     """
     # mean across traces (axis=0)
     return np.mean((pred_traces - true_trace)**2, axis=0)
-true_collections[model_run_name][:,:,19:, indxs.indx_m['act_lat']+2].shape
+
 def get_scenario_err(index_name, model_run_name):
     """
     Input shpae [m scenarios, n traces, steps_n, state_index]
@@ -150,20 +117,20 @@ def get_scenario_err(index_name, model_run_name):
     """
     posx_true = true_collections[model_run_name][:,:,19:, indxs.indx_m[index_name]+2]
     posx_pred = pred_collections[model_run_name][:,:,:, indxs.indx_m[index_name]]
-    if index_name == 'act_lat':
-        y_pred = np.zeros([300, 50, 21])
-        y_true = np.zeros([300, 1, 21])
-        for i in range(1, 21):
-            y_true[:, :, i] = y_true[:, :, i-1] + posx_true[:, :, i-1]*0.1
-            y_pred[:, :, i] = y_pred[:, :, i-1] + posx_pred[:, :, i-1]*0.1
-
-        posx_true = y_true
-        posx_pred = y_pred
-    for indx_ in [indxs.indx_y, indxs.indx_f, indxs.indx_fadj]:
-        posx_true = np.append(posx_true, \
-                true_collections[model_run_name][:,:,19:, indx_[index_name]+2], axis=0)
-        posx_pred = np.append(posx_pred, \
-                pred_collections[model_run_name][:,:,:, indx_[index_name]], axis=0)
+    # if index_name == 'act_lat':
+    #     y_pred = np.zeros([300, 50, 21])
+    #     y_true = np.zeros([300, 1, 21])
+    #     for i in range(1, 21):
+    #         y_true[:, :, i] = y_true[:, :, i-1] + posx_true[:, :, i-1]*0.1
+    #         y_pred[:, :, i] = y_pred[:, :, i-1] + posx_pred[:, :, i-1]*0.1
+    #
+    #     posx_true = y_true
+    #     posx_pred = y_pred
+    # for indx_ in [indxs.indx_y, indxs.indx_f, indxs.indx_fadj]:
+    #     posx_true = np.append(posx_true, \
+    #             true_collections[model_run_name][:,:,19:, indx_[index_name]+2], axis=0)
+    #     posx_pred = np.append(posx_pred, \
+    #             pred_collections[model_run_name][:,:,:, indx_[index_name]], axis=0)
 
     scenario_err_arr = []
     for m in range(posx_true.shape[0]):
@@ -192,14 +159,14 @@ for model_run_name in model_run_names:
     error_total = get_rwse(scenario_err_arr)
     long_speed.plot(time_steps, error_total, label=model_run_name)
 # model_run_names = ['h_lat_f_idm_act', 'h_lat_f_act', 'h_lat_act']
-
+    print(error_total.max())
 # legends = ['NIDM', 'Latent-Seq', 'Latent-Single', 'Latent-Single-o']
 long_speed.set_ylabel('RWSE Long.speed ($ms^{-1}$)', labelpad=10)
 # long_speed.set_xlabel('Time horizon (s)')
 long_speed.minorticks_off()
 # long_speed.set_ylim(0, 5)
 long_speed.set_xticklabels([])
-# d%%
+# %%
 
 """
 rwse lat_speed
